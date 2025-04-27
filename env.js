e9e92c7bfc9067fc0d9f328cdbba721457b0abd6
Kharay33455 <shakibkhan276337@gmail.com>
2025-04-27 01:32:09 +0100

    Updates

--- env.js
+++ env.js
@@ -1,5 +1,7 @@
 window.env = {
-  "REACT_APP_BH": "https://storerest.pythonanywhere.com",
-  "REACT_APP_HSH": "VIEW CATALOUGE",
-  "REACT_APP_CN": "CHERON"
+  "REACT_APP_BH": "http://127.0.0.1:8000",
+  "REACT_APP_HSH": "EXPLORE EXLIXIR",
+  "REACT_APP_CN": "ELIXIR",
+  "REACT_APP_ATCB": "rgba(150, 21, 107, 0.5)",
+  "REACT_APP_CCB": "#96156B"
 };
